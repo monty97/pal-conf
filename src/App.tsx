e16218dee01d1b2ab84d2c6c47d136c4176572c1
pal <mnaikya@gmail.com>
2024-02-16 12:56:35 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -679,17 +679,7 @@ function App() {
         <div className="w-full max-w-3xl mt-8 p-4 bg-white dark:bg-gray-800 rounded-lg shadow-md">
           <pre className="text-wrap break-all whitespace-pre-wrap text-sm text-gray-800 dark:text-gray-200">{settingsText}</pre>
         </div>
-        <div className="w-full max-w-3xl flex justify-center pt-2">
-          2024 @Bluefissure{" "}
-          <a
-            href="https://github.com/Bluefissure/pal-conf"
-            className="pl-2 font-medium text-primary underline underline-offset-4 top-2"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Github
-          </a>
-        </div>
+        
       </main>
     </>
   );
